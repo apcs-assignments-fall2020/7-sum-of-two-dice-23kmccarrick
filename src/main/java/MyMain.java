e2422558f2c56c23c277f35631905c876fec03fd
23kmccarrick <23kmccarrick@athenian.org>
2020-09-25 23:13:20 +0000

    fin

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,32 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int)(Math.random()*(6-1+1)+1);
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] moo = new int[11];
+        for(int i = 0; i < n; i ++){
+           for(int j = 2; j < 14; j ++){
+               if(rollDie()+rollDie()==j)
+                    moo[j-2]+=1;
+           }
+        }
+        return moo;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many pairs of dice would you like to roll?");
+        int n = scan.nextInt();
+        int[] argh = sumOfTwoDice(n);
+        System.out.println(argh);
+        for(int i = 0; i < 10; i++){
+            //System.out.println("% of rolls where sum == " + i+2 + ": " + (double)(n/argh[i]));
+        }
     }
 }
